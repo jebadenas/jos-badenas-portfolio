1b5e0c8a1b4225dd187f780594c49a4d5c2892be
jebadenas <jbad180@aucklanduni.ac.nz>
2025-05-19 21:03:15 +1200

    skeleton and some styling of navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-blue-700">
+      <button className="bg-green-400 italic text-white text-2xl px-6 py-3 rounded-r-2xl">
+        jos badenas
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
