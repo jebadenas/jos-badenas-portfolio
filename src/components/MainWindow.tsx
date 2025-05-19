11038674cd0662062c9d22d704fb5516735ac94b
jebadenas <jbad180@aucklanduni.ac.nz>
2025-05-19 21:39:10 +1200

    strucuture of main window

---
+++ src/components/MainWindow.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainWindow = () => {
+  return (
+    <div className="inline-flex flex-col border-4 border-blue-700 text-white">
+      <div className="bg-blue-700">home</div>
+      <div>
+        <p>hi! i'm jos</p>
+        <h2>cs student, aspiring software engineer</h2>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainWindow;
