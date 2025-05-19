9f8a273968df6ebcc9c8525d4d264add58a4671b
jebadenas <jbad180@aucklanduni.ac.nz>
2025-05-19 21:06:59 +1200

    added hover to lighter green for button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-blue-700">
-      <button className="bg-green-400 italic text-white text-2xl px-6 py-3 rounded-r-2xl">
+      <button className="bg-green-400 italic text-white text-2xl px-6 py-3 rounded-r-2xl hover:bg-green-300">
         jos badenas
       </button>
     </div>
