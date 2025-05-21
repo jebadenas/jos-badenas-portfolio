8ccd85a49d8dfc4f4381fdd280d3b058bcb75968
jebadenas <jbad180@aucklanduni.ac.nz>
2025-05-22 00:28:04 +1200

    compensated for bad font vertical spacing

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -25,11 +25,15 @@ const MainWindow = () => {
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-flex flex-col border-4 border-[#0055EA] text-white rounded-t-lg">
       <div className="bg-[#0055EA] p-1">home</div>
 
-      <div className="flex flex-col items-center py-[100px] px-[130px] gap-[10px] bg-white">
-        <p className="text-black text-9xl font-myFont ">hi! i&apos;m jos</p>
-        <h2 className="text-[#4C3535] text-3xl font-main">
-          cs student, aspiring software engineer
-        </h2>
+      <div className="flex flex-col items-center py-[100px] px-[130px] gap-[20px] bg-white">
+        <div className="flex flex-col items-center justify-content">
+          <p className="text-black text-9xl font-myFont mb-[-20px]">
+            hi! i&apos;m jos
+          </p>
+          <h2 className="text-[#4C3535] text-3xl font-main">
+            cs student, aspiring software engineer
+          </h2>
+        </div>
 
         <div className="flex flex-row gap-[30px]">
           {buttons.map((btn) => (
