768ed8959fec4286e92166a84e395d4b5042b16d
jebadenas <jbad180@aucklanduni.ac.nz>
2025-05-22 16:44:09 +1200

    👵

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div
       className={`select-none relative min-h-screen ${dmSansFont.variable} ${myFont.variable} bg-[#A7D5FF]`}
     >
-      <span>{`note jos badenas is still working on this project so it is super broken :d`}</span>
+      <span>{`note: jos badenas is still working on this project so it is super broken :d`}</span>
 
       <Background />
       <WindowMaster />
